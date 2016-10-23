6123a4b48c9c08dfaa1fd3ccd6f1afa202059cb1
Srijan Bajracharya <srijanbajracharya@lftechnology.com>
2016-10-23 23:02:40 +0200

    test jxrs

---
+++ menschen/src/main/java/com/insightfools/menschen/orm/JxRsActivator.java
@@ -0,0 +1,9 @@
+package com.insightfools.menschen.orm;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class JxRsActivator extends Application {
+
+}
