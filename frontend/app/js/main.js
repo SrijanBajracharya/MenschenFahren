a8fc7cb370d8f3482e33b701fb33d4a8bfff989b
Srijan Bajracharya <srijanbajracharya@lftechnology.com>
2016-11-11 21:51:58 +0100

    #MF-5 adds fonts and makes other necessary config changes

--- frontend/app/js/main.js
+++ frontend/app/js/main.js
@@ -9,163 +9,58 @@ var BrowserHistory = ReactRouter.browserHistory;
 var History = ReactRouter.hashHistory;
 
 require("../css/signup.css");
-require("../sass/files.sass");
+require("../sass/style.sass");
 
 var App = React.createClass({
-    render:function () {
+    render: function () {
         return(
-            <div>
-               <Header/>
+            <Listing/>
+        )
+    }
+});
+
+var ListingHeader = React.createClass({
+    render: function(){
+        return(
+            <div className="container">
+                <h2>Our Blog</h2>
+                <span>Get more information about the awesome cars</span>
             </div>
         )
     }
 });
 
-var NavigationBar = React.createClass({
-   render :function () {
-       return(
-       <div className="added-div">
-            <ul className="nav navbar-nav menu-left">
-                <li className="menu-mobile"><a href="#">Home</a></li>
-               <li className="menu-desktop">Marketplace</li>
-               <li className="menu-desktop"><a href="#">Boston</a></li>
-               <li className="menu-desktop"><a href="#">Portland</a></li>
-               <li className="menu-desktop"><a href="#">Providence</a></li>
-               <li className="menu-desktop"><a href="#">New York</a></li>
-               <li className="dropdown">
-                   <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Marketplace</a>
-                   <ul className="dropdown-menu" role="menu">
-                       <li><a href="#">Boston</a></li>
-                       <li><a href="#">Portland</a></li>
-                       <li><a href="#">Providence</a></li>
-                       <li><a href="#">New York</a></li>
-                    </ul>
-                </li>
+var ListingSection = React.createClass({
+    render: function(){
+        return(
+            <ul className="grid swipe-right" id="grid">
+                <li><a href="#"><img src={require('../images/logo.jpg')} alt="dummy"/><h3>A fantastic title</h3></a></li>
             </ul>
-            <ul className="nav navbar-nav menu-right">
-                <li className="menu-desktop">The Comapnay</li>
-                <li className="menu-desktop"><a href="#">Our Story</a></li>
-                <li className="menu-desktop"><a href="#">Features</a></li>
-                <li className="menu-desktop"><a href="#">FAQs</a></li>
-                <li className="menu-desktop"><a href="#">Careers</a></li>
-                <li className="menu-desktop"><a href="#">Contact Us</a></li>
-                <li className="dropdown company">
-                    <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">The Company</a>
-                    <ul className="dropdown-menu" role="menu">
-                        <li><a href="#">Our Story</a></li>
-                        <li><a href="#">Features</a></li>
-                        <li><a href="#">FAQs</a></li>
-                        <li><a href="#">Careers</a></li>
-                        <li><a href="#">Contact Us</a></li>
-                    </ul>
-                </li>
-                <li className="menu-mobile"><a href="#">Logout</a></li>
-            </ul>
-       </div>
-       )
-   }
-});
-
-var SecondaryMenu = React.createClass({
-   render : function(){
-       return(
-           <ul>
-                <li className="garage"><a href="#">My Garage</a></li>
-                <li className="dropdown profile">
-                    <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">John M.<span className="caret"></span></a>
-                    <ul className="dropdown-menu" role="menu">
-                        <li><a href="#">Profile</a></li>
-                        <li><a href="#">Logout</a></li>
-                    </ul>
-                </li>
-                <li className="dropdown lang">
-                    <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Language</a>
-                    <ul className="dropdown-menu" role="menu">
-                        <li><a href="#">English</a></li>
-                        <li><a href="#">Espanol</a></li>
-                        <li><a href="#">Portuguese</a></li>
-                    </ul>
-                </li>
-           </ul>
-       )
-   }
-});
-
-
-var SearchMenu = React.createClass({
-   render : function () {
-       return(
-           <div className="column">
-                <div id="sb-search" className="sb-search">
-                    <form>
-                       <input className="sb-search-input" placeholder="Enter your search term..." type="text" value="" name="search" id="search"/>
-                       <input className="sb-search-submit" type="submit" value=""/>
-                       <span className="sb-icon-search"></span>
-                    </form>
-                </div>
-           </div>
-       )
-   }
-});
-
-var HeaderButtons = React.createClass ({
-    render :function(){
-        return(
-            <button type="button" className="navbar-toggle visible-xs" data-toggle="push" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-            </button>
         )
     }
 });
 
-var NavBarToggle = React.createClass({
-    render : function () {
-        return(
-            <div id="show" className="navbar-toggle"><span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-            </div>
-
-        )
-    }
-});
-
-var Header = React.createClass({
-   render:function () {
+var Listing = React.createClass({
+   render: function(){
        return(
-       <header>
-            <div className="main-menu">
-                <nav className="navbar navbar-default">
-                    <div className="container-fluid">
-                        <div className="header-content">
-                            <div className="navbar-header">
-                                <HeaderButtons/>
-                                <NavBarToggle/>
-                                <div id="navbar" className="hideme menu">
-                                    <navigationBar/>
-                                    <a className="navbar-brand" href="#">
-                                        <img src={require('../images/logo.jpg')} alt="perpetulist" title="perpetulist"/>
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="secondary-menu">
-                                <SecondaryMenu/>
-                                <SearchMenu/>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-        </header>
+            <main className="no-padding">
+               <div className="blog-header">
+                   <ListingHeader/>
+               </div>
+               <div className="main-container">
+                   <div className="container-fluid">
+                       <div className="blog-holder">
+                           <div className="blog-container">
+                               <section className="grid-wrap">
+                                   <ListingSection/>
+                               </section>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+            </main>
        )
    }
-
 });
-
-
 
 ReactDom.render(<App/>, document.querySelector("#main"));
